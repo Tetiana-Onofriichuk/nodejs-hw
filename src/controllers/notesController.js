47843d8895edd450d0e556d976ae2b68c45f4754
Tetiana <Korostijtatiana@gmail.com>
2025-09-27 14:42:10 +0300

    add validation

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,33 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { page = 1, perPage = 10, tag, search } = req.query;
+  const skip = (page - 1) * perPage;
+
+  const filter = {};
+  if (tag) {
+    filter.tag = tag;
+  }
+  if (typeof search === 'string' && search.trim() !== '') {
+    const query = search.trim();
+    filter.$or = [
+      { title: { $regex: query, $options: 'i' } },
+      { content: { $regex: query, $options: 'i' } },
+    ];
+  }
+  const [totalNotes, notes] = await Promise.all([
+    Note.countDocuments(filter),
+    Note.find(filter).skip(skip).limit(perPage).lean(),
+  ]);
+  const totalPages = Math.max(1, Math.ceil(totalNotes / perPage));
+
+  return res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res, next) => {
---
+++ src/validations/notesValidation.js
@@ -0,0 +1,58 @@
+import { Joi, Segments } from 'celebrate';
+import { isValidObjectId } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+
+const objectIdValidator = (value, helpers) => {
+  return !isValidObjectId(value) ? helpers.message('Invalid id format') : value;
+};
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string()
+      .valid(...TAGS)
+      .optional(),
+    search: Joi.string().trim().allow(''),
+  }),
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required().messages({
+      'string.base': 'Title must be a string',
+      'string.min': 'Title should have at least {#limit} character(s)',
+      'any.required': 'Title is required',
+    }),
+
+    content: Joi.string().allow('').messages({
+      'string.base': 'Content must be a string',
+    }),
+
+    tag: Joi.string()
+      .valid(...TAGS)
+      .required()
+      .messages({
+        'any.only':
+          'Tag must be one of: Work,Personal,Meeting,Shopping,Ideas,Travel,Finance,Health,Important,Todo',
+        'any.required': 'Tag is required',
+      }),
+  }),
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).max(30),
+    content: Joi.string().allow(''),
+    tag: Joi.string().valid(...TAGS),
+  }).min(1),
+};

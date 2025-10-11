756bec37cd22ccd91b73305048276359fbd3e290
Tetiana <Korostijtatiana@gmail.com>
2025-10-11 13:00:21 +0300

    Normalize line endings to LF via .gitattributes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,12 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendEmail.js';
+
+import Handlebars from 'handlebars';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -90,4 +96,52 @@ export const refreshUserSession = async (req, res, next) => {
   res.status(200).json({
     message: 'Session refreshed',
   });
+};
+
+export const requestResetEmail = async (req, res, next) => {
+  try {
+    const templatePath = path.join(
+      process.cwd(),
+      'src',
+      'templates',
+      'reset-password-email.html',
+    );
+    const templateSource = await fs.readFile(templatePath, 'utf-8');
+    const renderResetEmail = Handlebars.compile(templateSource);
+
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(createHttpError(404, 'User not found'));
+    }
+
+    const token = jwt.sign(
+      { sub: user._id.toString(), email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '15m' },
+    );
+
+    const link = `${process.env.FRONTEND_DOMAIN}/reset-password?token=${token}`;
+
+    const html = renderResetEmail({
+      name: user.name ?? 'there',
+      link,
+    });
+
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+      html,
+    });
+
+    return res.status(200).json({
+      message: 'Password reset email sent successfully',
+    });
+  } catch {
+    return next(
+      createHttpError(500, 'Failed to send the email, please try again later.'),
+    );
+  }
 };
--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());

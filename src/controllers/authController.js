fe5654b6066613b5234b55cfc3bc531e26b5db48
Tetiana <Korostijtatiana@gmail.com>
2025-10-14 09:32:26 +0300

    add files

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -54,22 +54,18 @@ export const loginUser = async (req, res, next) => {
   res.status(200).json(user);
 };
 
-export const logoutUser = async (req, res, next) => {
-  try {
-    const { sessionId } = req.cookies;
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
 
-    if (sessionId) {
-      await Session.deleteOne({ _id: sessionId });
-    }
-
-    res.clearCookie('sessionId');
-    res.clearCookie('accessToken');
-    res.clearCookie('refreshToken');
-
-    return res.sendStatus(204);
-  } catch (err) {
-    return next(err);
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
   }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
 };
 
 export const refreshUserSession = async (req, res, next) => {
